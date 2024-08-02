2d90f88f80f7df5da24c8f93ec050f9a33350fa4
gitnol <github@noldi.de>
2024-08-03 00:29:07 +0200

    added references to other stackoverflow and reddit

--- Active-Directory/check-inactive-sessions.ps1
+++ Active-Directory/check-inactive-sessions.ps1
@@ -1,6 +1,17 @@
 # I have added this script to reddit on 20240803
 # https://www.reddit.com/r/PowerShell/comments/8r56tr/getting_idle_time_for_logged_on_domain_users/
 
+# Also added it as answer here:
+# https://stackoverflow.com/questions/1219050/getlastinputinfo-is-user-specific-is-there-something-similar-which-gives-machi/78827409#78827409
+
+# You can use an indirect method and use ReadOperationCount from the csrss.exe process in respect to the session id
+# csrss will change its IO Reads with input to the keyboard or mouse
+# I have created sample code, in which you measure the ReadOperationCount of all sessions at two specific points in time and then compare the two objects.
+# If something has changed, at least one session was NOT idle.
+# If nothing has changed, ALL sessions were idle.
+
+# You must have administrative priviledges
+
 function Get-SessionIDReadIOCounts {
     # Query Win32_Process to get all processes and their session IDs
     $processes = Get-CimInstance -ClassName Win32_Process
