cfbe4c6d4c91a5bc7b0dd270da6670b3e013159d
gitnol <github@noldi.de>
2024-08-07 08:32:11 +0200

    some changes

--- Server-Client-Helper-Stuff/get-UserSessionInfos__remove-UserSessions.ps1
+++ Server-Client-Helper-Stuff/get-UserSessionInfos__remove-UserSessions.ps1
@@ -48,6 +48,42 @@ function Remove-UserSessions {
     return $allObj
 }
 
+# # Old Version
+# function Get-QueryUserSessions_old {
+#     param (
+#         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+#         [string[]]$server,
+#         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
+#         [PSCredential]$credentials
+#     )
+#     $allObj = @()
+#     $allObj += Invoke-Command -ComputerName $server -Credential $credentials -ScriptBlock {
+#         $allSessions = (query user);
+#         $myObj = @()
+#         $allSessions | Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*AKTIV*" } | ForEach-Object {
+#             $myObj += [PSCustomObject]@{
+#                 BENUTZERNAME = ($_ -split "\s+")[1]
+#                 SITZUNGSNAME = ($_ -split "\s+")[2]
+#                 ID           = ($_ -split "\s+")[3]
+#                 STATUS       = ($_ -split "\s+")[4]
+#                 LEERLAUF     = ($_ -split "\s+")[5]
+#                 ANMELDEZEIT  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7])
+#             }
+#         }
+#         $allSessions | Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*GETR*" } | ForEach-Object {
+#             $myObj += [PSCustomObject]@{
+#                 BENUTZERNAME = ($_ -split "\s+")[1]
+#                 SITZUNGSNAME = "" 
+#                 ID           = ($_ -split "\s+")[2]
+#                 STATUS       = ($_ -split "\s+")[3]
+#                 LEERLAUF     = ($_ -split "\s+")[4]
+#                 ANMELDEZEIT  = (($_ -split "\s+")[5] + " " + ($_ -split "\s+")[6])
+#             }
+#         }    
+#         return $myObj
+#     }
+#     return $allObj
+# }
 
 function Get-QueryUserSessions {
     param (
@@ -60,68 +96,30 @@ function Get-QueryUserSessions {
     $allObj += Invoke-Command -ComputerName $server -Credential $credentials -ScriptBlock {
         $allSessions = (query user);
         $myObj = @()
-        $allSessions | Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*AKTIV*" } | ForEach-Object {
-            $myObj += [PSCustomObject]@{
-                BENUTZERNAME = ($_ -split "\s+")[1]
-                SITZUNGSNAME = ($_ -split "\s+")[2]
-                ID           = ($_ -split "\s+")[3]
-                STATUS       = ($_ -split "\s+")[4]
-                LEERLAUF     = ($_ -split "\s+")[5]
-                ANMELDEZEIT  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7])
+        $allSessions | Select-Object -skip 1 |
+		ForEach-Object {
+            if(($_ -split "\s+").count -eq 8){       # CONNECTED     = 8 columns
+                $myObj += [PSCustomObject]@{
+                    USERNAME = ($_ -split "\s+")[1]
+                    SESSIONNAME = ($_ -split "\s+")[2]
+                    ID           = ($_ -split "\s+")[3]
+                    STATUS       = ($_ -split "\s+")[4]
+                    IDLETIME     = ($_ -split "\s+")[5]
+                    LOGONTIME  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7])
+                }
+            } elseif(($_ -split "\s+").count -eq 7){ # DISCONNTECTED = 7 columns
+                $myObj += [PSCustomObject]@{
+                    USERNAME = ($_ -split "\s+")[1]
+                    SESSIONNAME = "" 
+                    ID           = ($_ -split "\s+")[2]
+                    STATUS       = ($_ -split "\s+")[3]
+                    IDLETIME     = ($_ -split "\s+")[4]
+                    LOGONTIME  = (($_ -split "\s+")[5] + " " + ($_ -split "\s+")[6])
+                }
+            } else {
+                Write-Error("The amount of columns is neither 7 nor 8 at this column:" + $_)
             }
         }
-        $allSessions | Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*GETR*" } | ForEach-Object {
-            $myObj += [PSCustomObject]@{
-                BENUTZERNAME = ($_ -split "\s+")[1]
-                SITZUNGSNAME = "" 
-                ID           = ($_ -split "\s+")[2]
-                STATUS       = ($_ -split "\s+")[3]
-                LEERLAUF     = ($_ -split "\s+")[4]
-                ANMELDEZEIT  = (($_ -split "\s+")[5] + " " + ($_ -split "\s+")[6])
-            }
-        }    
-        return $myObj
-    }
-    return $allObj
-}
-
-function Get-QueryUserSessions2 {
-    param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
-        [string[]]$server,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
-        [PSCredential]$credentials
-    )
-    $allObj = @()
-    $allObj += Invoke-Command -ComputerName $server -Credential $credentials -ScriptBlock {
-        $allSessions = (query user);
-        $myObj = @()
-        $allSessions | Select-Object -skip 1 |
-		# Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*AKTIV*" } | 
-		ForEach-Object {
-            Write-Host(($_ -split "\s+").count) # TODO anhand der menge entscheiden ob verbunden ist oder getrennt!
-            $myObj += [PSCustomObject]@{
-                USERNAME = ($_ -split "\s+")[1]
-                SESSIONNAME = ($_ -split "\s+")[2]
-                ID           = ($_ -split "\s+")[3]
-                STATUS       = ($_ -split "\s+")[4]
-                IDLETIME     = ($_ -split "\s+")[5]
-                LOGONTIME  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7])
-            }
-        }
-        $allSessions | Select-Object -skip 1 |
-		# Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*GETR*" } | 
-		ForEach-Object {
-            Write-Host(($_ -split "\s+").count)
-            $myObj += [PSCustomObject]@{
-                USERNAME = ($_ -split "\s+")[1]
-                SESSIONNAME = "" 
-                ID           = ($_ -split "\s+")[2]
-                STATUS       = ($_ -split "\s+")[3]
-                IDLETIME     = ($_ -split "\s+")[4]
-                LOGONTIME  = (($_ -split "\s+")[5] + " " + ($_ -split "\s+")[6])
-            }
-        }    
         return $myObj
     }
     return $allObj
@@ -136,6 +134,5 @@ $cred = Get-Credential -Message "Please input adminstrative priviledges on the t
 
 if ($cred) {
     Get-QueryUserSessions -server $server -credentials $cred
-    Get-QueryUserSessions2 -server $server -credentials $cred
     Remove-UserSessions -server $server -credentials $cred #-all
 }
