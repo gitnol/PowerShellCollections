0b9789cdd17f805bc930287175b35f31b39b4c5b
gitnol <github@noldi.de>
2024-08-11 22:19:45 +0200

    formatted

--- unsorted-stuff/Get-MailStore-API-Function-Reference.ps1
+++ unsorted-stuff/Get-MailStore-API-Function-Reference.ps1
@@ -19,27 +19,27 @@ $h2NodesCount = ($h2Nodes).Count
 $start = 0
 $myfunctions = @()
 Do {
-	$currentNode = $start
-	$nextNode = $start + 1
-	Write-Host($currentNode)
-	$firstH2 = $h2Nodes[$currentNode]
-	$nextH2 = $h2Nodes[$nextNode]
+    $currentNode = $start
+    $nextNode = $start + 1
+    Write-Host($currentNode)
+    $firstH2 = $h2Nodes[$currentNode]
+    $nextH2 = $h2Nodes[$nextNode]
 	
-	# Get content between the first and the second h2
-	$contentBetween = @()
-	$currentNode = $firstH2
-	$functionName = $firstH2.span.'#text'
+    # Get content between the first and the second h2
+    $contentBetween = @()
+    $currentNode = $firstH2
+    $functionName = $firstH2.span.'#text'
 
-	while ($currentNode = $currentNode.NextSibling) {
-		if ($currentNode -eq $nextH2) {
-			break
-		}
-		$contentBetween += $currentNode.OuterXml
-	}
-	$myfunction = [xml]('<function name="'+$functionName+'">' + $contentBetween + '</function>')
-	$myfunctions += $myfunction
+    while ($currentNode = $currentNode.NextSibling) {
+        if ($currentNode -eq $nextH2) {
+            break
+        }
+        $contentBetween += $currentNode.OuterXml
+    }
+    $myfunction = [xml]('<function name="' + $functionName + '">' + $contentBetween + '</function>')
+    $myfunctions += $myfunction
 
-	$start = $nextNode
+    $start = $nextNode
 } While ($nextNode -lt $h2NodesCount)
 
 
@@ -52,16 +52,16 @@ Do {
 # # Das folgende lieferte den Tag Ablauf (der ersten Ebene) innerhalb der funktion
 # $allefunktionsaufbau = @()
 # foreach ($function in ($myfunctions.function | select -first 100)) {
-	# $xml = [xml]$function.OuterXml
-	# $funktionsaufbau = @()
-	# foreach ($node in $xml.DocumentElement.ChildNodes) {
-		# if ($node.Name -ne '#comment') {
-		# $funktionsaufbau += [pscustomobject]@{
-			# NodeName = $node.Name
-			# }
-		# }
-	# }
-	# $allefunktionsaufbau += [pscustomobject]@{funktionsaufbau = $funktionsaufbau}
+# $xml = [xml]$function.OuterXml
+# $funktionsaufbau = @()
+# foreach ($node in $xml.DocumentElement.ChildNodes) {
+# if ($node.Name -ne '#comment') {
+# $funktionsaufbau += [pscustomobject]@{
+# NodeName = $node.Name
+# }
+# }
+# }
+# $allefunktionsaufbau += [pscustomobject]@{funktionsaufbau = $funktionsaufbau}
 # }
 # # Hiermit bekommt man den Aufbau , aber nur UNIQUE... also wie viele Verschiedene Aufbauten der tags gibt es.
 # $allefunktionsaufbau | Sort-Object -Unique -Property funktionsaufbau | % {Write-Host("START");Write-Host($_.funktionsaufbau.NodeName) -ForegroundColor Green;Write-Host("ENDE")}
@@ -80,158 +80,168 @@ Do {
 
 $myNewFunctionDefinitions = @()
 foreach ($function in ($myfunctions.function)) {
-# foreach ($function in ($myfunctions.function | select -first 1000)) {
-	$xml = [xml]$function.OuterXml
-	$functionName = $xml.function.name.trim()
-	$functionDescription = ''
-	$h3TableDescription = ''
-	# $h4TableDescription = ''
-	$ArgumentsValuesDescription = ''
-	$myNewFunctionDefinition = [pscustomobject]@{
-		Name = $functionName
-		Description = $functionDescription
-		ArgumentsTable = @()
-		ArgumentsTableDescription = $h3TableDescription
-		ArgumentsValuesTable = @()
-		# ArgumentsValuesTableDescription = $h4TableDescription
-		ArgumentsValuesDescription = $ArgumentsValuesDescription
-	}
+    # foreach ($function in ($myfunctions.function | select -first 1000)) {
+    $xml = [xml]$function.OuterXml
+    $functionName = $xml.function.name.trim()
+    $functionDescription = ''
+    $h3TableDescription = ''
+    # $h4TableDescription = ''
+    $ArgumentsValuesDescription = ''
+    $myNewFunctionDefinition = [pscustomobject]@{
+        Name                       = $functionName
+        Description                = $functionDescription
+        ArgumentsTable             = @()
+        ArgumentsTableDescription  = $h3TableDescription
+        ArgumentsValuesTable       = @()
+        # ArgumentsValuesTableDescription = $h4TableDescription
+        ArgumentsValuesDescription = $ArgumentsValuesDescription
+    }
 
-	$NodeNamehistory = '' # Zurücksetzen der Tag History, damit man genau sieht, wo man sich gerade befindet und switch Unterscheidungen machen kann. Dann kann man den Inhalt entsprechend zuweisen.
-	# $lastNodeName = ''
-	$ArgumentValueName = ''
-	foreach ($node in $xml.DocumentElement.ChildNodes) {
-		$NodeNamehistory = ($NodeNamehistory + " " + $node.Name).trim()
-		switch ($node.Name) {
-			"p" {
-				switch($NodeNamehistory){
-					"p" { # Function Description
-						$myNewFunctionDefinition.Description = $node.InnerText.trim()
-					}
-					"p p" { # Add to Function Description
-						$myNewFunctionDefinition.Description += "`r`n" + $node.InnerText.trim()
-					}
-					"p h3 table p" { # ArgumentsTableDescription
-						$myNewFunctionDefinition.ArgumentsTableDescription = $node.InnerText.trim()
-					}
-					"p h3 table p p" {
-						$myNewFunctionDefinition.ArgumentsTableDescription += "`r`n" + $node.InnerText.trim()
-					}
-					"p h3 table h3 h4 p" {
-						$myNewFunctionDefinition.ArgumentsValuesDescription = $node.InnerText.trim()
-					}
-					# "p h3 table h3 h4 table p" {
-						# # Write-Host($functionName + ":" + $ArgumentValueName) -ForegroundColor Red
-						# # Write-Host("p h3 table h3 h4 table p") -ForegroundColor Red
-						# # Write-Host($node.InnerText.trim()) -ForegroundColor Red # nur ein FUcking nochmal Zeilenumbruch im p tag unter Create User :-(
-						# $myNewFunctionDefinition.ArgumentsValuesTableDescription = $node.InnerText.trim()
-					# }
-				}
-			}
-			"div" {
-				switch($NodeNamehistory){
-					"p h3 table h3 h4 div" { # Set ArgumentsValuesDescription
-						$myNewFunctionDefinition.ArgumentsValuesDescription = $node.InnerText.trim()
-					}
-					"p h3 table h3 h4 p div" { # Add to ArgumentsValuesDescription
-						$myNewFunctionDefinition.ArgumentsValuesDescription += "`r`n" + $node.InnerText.trim()
-					}
-				}
-			}			
-			"h3" {
-				switch($NodeNamehistory){ # hier muss nichts gemacht werden.
-					"p h3" {}
-					"p h3 table h3" {}
-					"p h3 table p h3" {}
-				}
-			}
-			"h4" {
-				switch($NodeNamehistory){ # hier muss nichts gemacht werden.
-					"p h3 table h3 h4" {
-						$ArgumentValueName = $node.InnerText.trim()
-					}
-					"p h3 table h3 h4 table h4" {
-						$ArgumentValueName = $node.InnerText.trim()
-					}
-					"p h3 table h3 h4 table p h4" {
-						$ArgumentValueName = $node.InnerText.trim()
-					}
-					"p h3 table p h3 h4" {
-						$ArgumentValueName = $node.InnerText.trim()
-					}
-					"p h3 table p h3 h4 table h4" {
-						$ArgumentValueName = $node.InnerText.trim()
-					}
-					"p h3 table p h3 h4 table h4 table h4" {
-						$ArgumentValueName = $node.InnerText.trim()
-					}
-				}				
-			}
-			"table" {
-				$table = [pscustomobject]@{
-					TableRows = @()
-					TableDescription = ''
-				}
-				$Zeilen = $node.tbody.tr
-				$anzahlSpalten = $Zeilen[0].ChildNodes.count
-				$Zeilen | Select-Object -Skip 1 | ForEach-Object {
-					$spalte1 = $_.ChildNodes[0].InnerText.trim()
-					$spalte2 = $_.ChildNodes[1].InnerText.trim()
-					if ($anzahlSpalten -ge 3){ # Arguments Table
-						$spalte3 = $_.ChildNodes[2].InnerText.trim()
-						$nodeTableRow = [pscustomobject]@{
-							ArgumentName = $spalte1
-							ArgumentType = $spalte2
-							ArgumentDescription = $spalte3
-							Mandatory = (-not ($spalte2 -like "*(optional)*"))
-							ArgumentValuesTable = @() # Neu
-						}
-					}
-					if ($anzahlSpalten -eq 2){ # Argument Values Table
-						$nodeTableRow = [pscustomobject]@{
-							ArgumentName = $ArgumentValueName
-							ArgumentNameValidItem = $spalte1
-							ArgumentNameValidItemDescription = $spalte2
-						}
-					}
-					$table.TableRows += $nodeTableRow
-				}
+    $NodeNamehistory = '' # Zurücksetzen der Tag History, damit man genau sieht, wo man sich gerade befindet und switch Unterscheidungen machen kann. Dann kann man den Inhalt entsprechend zuweisen.
+    # $lastNodeName = ''
+    $ArgumentValueName = ''
+    foreach ($node in $xml.DocumentElement.ChildNodes) {
+        $NodeNamehistory = ($NodeNamehistory + " " + $node.Name).trim()
+        switch ($node.Name) {
+            "p" {
+                switch ($NodeNamehistory) {
+                    "p" {
+                        # Function Description
+                        $myNewFunctionDefinition.Description = $node.InnerText.trim()
+                    }
+                    "p p" {
+                        # Add to Function Description
+                        $myNewFunctionDefinition.Description += "`r`n" + $node.InnerText.trim()
+                    }
+                    "p h3 table p" {
+                        # ArgumentsTableDescription
+                        $myNewFunctionDefinition.ArgumentsTableDescription = $node.InnerText.trim()
+                    }
+                    "p h3 table p p" {
+                        $myNewFunctionDefinition.ArgumentsTableDescription += "`r`n" + $node.InnerText.trim()
+                    }
+                    "p h3 table h3 h4 p" {
+                        $myNewFunctionDefinition.ArgumentsValuesDescription = $node.InnerText.trim()
+                    }
+                    # "p h3 table h3 h4 table p" {
+                    # # Write-Host($functionName + ":" + $ArgumentValueName) -ForegroundColor Red
+                    # # Write-Host("p h3 table h3 h4 table p") -ForegroundColor Red
+                    # # Write-Host($node.InnerText.trim()) -ForegroundColor Red # nur ein FUcking nochmal Zeilenumbruch im p tag unter Create User :-(
+                    # $myNewFunctionDefinition.ArgumentsValuesTableDescription = $node.InnerText.trim()
+                    # }
+                }
+            }
+            "div" {
+                switch ($NodeNamehistory) {
+                    "p h3 table h3 h4 div" {
+                        # Set ArgumentsValuesDescription
+                        $myNewFunctionDefinition.ArgumentsValuesDescription = $node.InnerText.trim()
+                    }
+                    "p h3 table h3 h4 p div" {
+                        # Add to ArgumentsValuesDescription
+                        $myNewFunctionDefinition.ArgumentsValuesDescription += "`r`n" + $node.InnerText.trim()
+                    }
+                }
+            }			
+            "h3" {
+                switch ($NodeNamehistory) {
+                    # hier muss nichts gemacht werden.
+                    "p h3" {}
+                    "p h3 table h3" {}
+                    "p h3 table p h3" {}
+                }
+            }
+            "h4" {
+                switch ($NodeNamehistory) {
+                    # hier muss nichts gemacht werden.
+                    "p h3 table h3 h4" {
+                        $ArgumentValueName = $node.InnerText.trim()
+                    }
+                    "p h3 table h3 h4 table h4" {
+                        $ArgumentValueName = $node.InnerText.trim()
+                    }
+                    "p h3 table h3 h4 table p h4" {
+                        $ArgumentValueName = $node.InnerText.trim()
+                    }
+                    "p h3 table p h3 h4" {
+                        $ArgumentValueName = $node.InnerText.trim()
+                    }
+                    "p h3 table p h3 h4 table h4" {
+                        $ArgumentValueName = $node.InnerText.trim()
+                    }
+                    "p h3 table p h3 h4 table h4 table h4" {
+                        $ArgumentValueName = $node.InnerText.trim()
+                    }
+                }				
+            }
+            "table" {
+                $table = [pscustomobject]@{
+                    TableRows        = @()
+                    TableDescription = ''
+                }
+                $Zeilen = $node.tbody.tr
+                $anzahlSpalten = $Zeilen[0].ChildNodes.count
+                $Zeilen | Select-Object -Skip 1 | ForEach-Object {
+                    $spalte1 = $_.ChildNodes[0].InnerText.trim()
+                    $spalte2 = $_.ChildNodes[1].InnerText.trim()
+                    if ($anzahlSpalten -ge 3) {
+                        # Arguments Table
+                        $spalte3 = $_.ChildNodes[2].InnerText.trim()
+                        $nodeTableRow = [pscustomobject]@{
+                            ArgumentName        = $spalte1
+                            ArgumentType        = $spalte2
+                            ArgumentDescription = $spalte3
+                            Mandatory           = (-not ($spalte2 -like "*(optional)*"))
+                            ArgumentValuesTable = @() # Neu
+                        }
+                    }
+                    if ($anzahlSpalten -eq 2) {
+                        # Argument Values Table
+                        $nodeTableRow = [pscustomobject]@{
+                            ArgumentName                     = $ArgumentValueName
+                            ArgumentNameValidItem            = $spalte1
+                            ArgumentNameValidItemDescription = $spalte2
+                        }
+                    }
+                    $table.TableRows += $nodeTableRow
+                }
 				
-				switch($NodeNamehistory){
-					"p h3 table" { # Dies ist 
-						$myNewFunctionDefinition.ArgumentsTable += $table
-					}	
-					"p h3 table h3 h4 table" {
-						$myNewFunctionDefinition.ArgumentsValuesTable += $table
-						# Zu jedem Argument die entsprechend erlaubten Werte als Tabelle mit abspeichern.
-						(($myNewFunctionDefinition | Where-Object {$_.Name -eq $functionName}).ArgumentsTable.TableRows | Where-Object {$_.ArgumentName -eq $ArgumentValueName}).ArgumentValuesTable += $table
-					}
-					"p h3 table h3 h4 table h4 table" {
-						$myNewFunctionDefinition.ArgumentsValuesTable += $table
-						(($myNewFunctionDefinition | Where-Object {$_.Name -eq $functionName}).ArgumentsTable.TableRows | Where-Object {$_.ArgumentName -eq $ArgumentValueName}).ArgumentValuesTable += $table
-					}
-					"p h3 table h3 h4 table p h4 table" {
-						$myNewFunctionDefinition.ArgumentsValuesTable += $table
-						(($myNewFunctionDefinition | Where-Object {$_.Name -eq $functionName}).ArgumentsTable.TableRows | Where-Object {$_.ArgumentName -eq $ArgumentValueName}).ArgumentValuesTable += $table
-					}
-					"p h3 table p h3 h4 table" {
-						$myNewFunctionDefinition.ArgumentsValuesTable += $table
-						(($myNewFunctionDefinition | Where-Object {$_.Name -eq $functionName}).ArgumentsTable.TableRows | Where-Object {$_.ArgumentName -eq $ArgumentValueName}).ArgumentValuesTable += $table
-					}
-					"p h3 table p h3 h4 table h4 table" {
-						$myNewFunctionDefinition.ArgumentsValuesTable += $table
-						(($myNewFunctionDefinition | Where-Object {$_.Name -eq $functionName}).ArgumentsTable.TableRows | Where-Object {$_.ArgumentName -eq $ArgumentValueName}).ArgumentValuesTable += $table
-					}
-					"p h3 table p h3 h4 table h4 table h4 table" {
-						$myNewFunctionDefinition.ArgumentsValuesTable += $table
-						(($myNewFunctionDefinition | Where-Object {$_.Name -eq $functionName}).ArgumentsTable.TableRows | Where-Object {$_.ArgumentName -eq $ArgumentValueName}).ArgumentValuesTable += $table
-					}
-				}
-				$ArgumentValueName = '' # Zurücksetzen.
-			}
-		}
-	}
-	$myNewFunctionDefinitions +=$myNewFunctionDefinition
+                switch ($NodeNamehistory) {
+                    "p h3 table" {
+                        # Dies ist 
+                        $myNewFunctionDefinition.ArgumentsTable += $table
+                    }	
+                    "p h3 table h3 h4 table" {
+                        $myNewFunctionDefinition.ArgumentsValuesTable += $table
+                        # Zu jedem Argument die entsprechend erlaubten Werte als Tabelle mit abspeichern.
+						(($myNewFunctionDefinition | Where-Object { $_.Name -eq $functionName }).ArgumentsTable.TableRows | Where-Object { $_.ArgumentName -eq $ArgumentValueName }).ArgumentValuesTable += $table
+                    }
+                    "p h3 table h3 h4 table h4 table" {
+                        $myNewFunctionDefinition.ArgumentsValuesTable += $table
+						(($myNewFunctionDefinition | Where-Object { $_.Name -eq $functionName }).ArgumentsTable.TableRows | Where-Object { $_.ArgumentName -eq $ArgumentValueName }).ArgumentValuesTable += $table
+                    }
+                    "p h3 table h3 h4 table p h4 table" {
+                        $myNewFunctionDefinition.ArgumentsValuesTable += $table
+						(($myNewFunctionDefinition | Where-Object { $_.Name -eq $functionName }).ArgumentsTable.TableRows | Where-Object { $_.ArgumentName -eq $ArgumentValueName }).ArgumentValuesTable += $table
+                    }
+                    "p h3 table p h3 h4 table" {
+                        $myNewFunctionDefinition.ArgumentsValuesTable += $table
+						(($myNewFunctionDefinition | Where-Object { $_.Name -eq $functionName }).ArgumentsTable.TableRows | Where-Object { $_.ArgumentName -eq $ArgumentValueName }).ArgumentValuesTable += $table
+                    }
+                    "p h3 table p h3 h4 table h4 table" {
+                        $myNewFunctionDefinition.ArgumentsValuesTable += $table
+						(($myNewFunctionDefinition | Where-Object { $_.Name -eq $functionName }).ArgumentsTable.TableRows | Where-Object { $_.ArgumentName -eq $ArgumentValueName }).ArgumentValuesTable += $table
+                    }
+                    "p h3 table p h3 h4 table h4 table h4 table" {
+                        $myNewFunctionDefinition.ArgumentsValuesTable += $table
+						(($myNewFunctionDefinition | Where-Object { $_.Name -eq $functionName }).ArgumentsTable.TableRows | Where-Object { $_.ArgumentName -eq $ArgumentValueName }).ArgumentValuesTable += $table
+                    }
+                }
+                $ArgumentValueName = '' # Zurücksetzen.
+            }
+        }
+    }
+    $myNewFunctionDefinitions += $myNewFunctionDefinition
 }
 
 $myNewFunctionDefinitions
