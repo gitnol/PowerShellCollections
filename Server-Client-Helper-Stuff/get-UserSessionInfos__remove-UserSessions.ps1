dfb39c3a5edad77e455990790a02777dcb191e73
gitnol <github@noldi.de>
2024-08-14 00:29:33 +0200

    some error corrections

--- Server-Client-Helper-Stuff/get-UserSessionInfos__remove-UserSessions.ps1
+++ Server-Client-Helper-Stuff/get-UserSessionInfos__remove-UserSessions.ps1
@@ -24,6 +24,16 @@ function Remove-UserSessions {
                     ANMELDEZEIT  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7])
                 }
             }
+            $allSessions | Where-Object { $_ -notlike "*STATUS*" -and $_ -like "*Active*" } | ForEach-Object {
+                $myObj += [PSCustomObject]@{
+                    BENUTZERNAME = ($_ -split "\s+")[1]
+                    SITZUNGSNAME = ($_ -split "\s+")[2]
+                    ID           = ($_ -split "\s+")[3]
+                    STATUS       = ($_ -split "\s+")[4]
+                    LEERLAUF     = ($_ -split "\s+")[5]
+                    ANMELDEZEIT  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7]+ " " + ($_ -split "\s+")[8])
+                }
+            }
         }
 
         $allSessions | Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*GETR*" } | ForEach-Object {
@@ -48,44 +58,7 @@ function Remove-UserSessions {
     return $allObj
 }
 
-# # Old Version
-# function Get-QueryUserSessions_old {
-#     param (
-#         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
-#         [string[]]$server,
-#         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
-#         [PSCredential]$credentials
-#     )
-#     $allObj = @()
-#     $allObj += Invoke-Command -ComputerName $server -Credential $credentials -ScriptBlock {
-#         $allSessions = (query user);
-#         $myObj = @()
-#         $allSessions | Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*AKTIV*" } | ForEach-Object {
-#             $myObj += [PSCustomObject]@{
-#                 BENUTZERNAME = ($_ -split "\s+")[1]
-#                 SITZUNGSNAME = ($_ -split "\s+")[2]
-#                 ID           = ($_ -split "\s+")[3]
-#                 STATUS       = ($_ -split "\s+")[4]
-#                 LEERLAUF     = ($_ -split "\s+")[5]
-#                 ANMELDEZEIT  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7])
-#             }
-#         }
-#         $allSessions | Where-Object { $_ -notlike "*SITZUNGSNAME*" -and $_ -like "*GETR*" } | ForEach-Object {
-#             $myObj += [PSCustomObject]@{
-#                 BENUTZERNAME = ($_ -split "\s+")[1]
-#                 SITZUNGSNAME = "" 
-#                 ID           = ($_ -split "\s+")[2]
-#                 STATUS       = ($_ -split "\s+")[3]
-#                 LEERLAUF     = ($_ -split "\s+")[4]
-#                 ANMELDEZEIT  = (($_ -split "\s+")[5] + " " + ($_ -split "\s+")[6])
-#             }
-#         }    
-#         return $myObj
-#     }
-#     return $allObj
-# }
-
-function Get-QueryUserSessions {
+function Get-UserSessions {
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
         [string[]]$server,
@@ -94,11 +67,21 @@ function Get-QueryUserSessions {
     )
     $allObj = @()
     $allObj += Invoke-Command -ComputerName $server -Credential $credentials -ScriptBlock {
+        $computer = $env:COMPUTERNAME
         $allSessions = (query user);
         $myObj = @()
         $allSessions | Select-Object -skip 1 |
 		ForEach-Object {
-            if(($_ -split "\s+").count -eq 8){       # CONNECTED     = 8 columns
+            if(($_ -split "\s+").count -eq 9){       # CONNECTED     = 9 columns English
+                $myObj += [PSCustomObject]@{
+                    USERNAME = ($_ -split "\s+")[1]
+                    SESSIONNAME = ($_ -split "\s+")[2]
+                    ID           = ($_ -split "\s+")[3]
+                    STATUS       = ($_ -split "\s+")[4]
+                    IDLETIME     = ($_ -split "\s+")[5]
+                    LOGONTIME  = (($_ -split "\s+")[6] + " " + ($_ -split "\s+")[7] + " " + ($_ -split "\s+")[8])
+                }
+            } elseif(($_ -split "\s+").count -eq 8){       # CONNECTED     = 8 columns
                 $myObj += [PSCustomObject]@{
                     USERNAME = ($_ -split "\s+")[1]
                     SESSIONNAME = ($_ -split "\s+")[2]
@@ -117,7 +100,7 @@ function Get-QueryUserSessions {
                     LOGONTIME  = (($_ -split "\s+")[5] + " " + ($_ -split "\s+")[6])
                 }
             } else {
-                Write-Error("The amount of columns is neither 7 nor 8 at this column:")
+                Write-Error("{0} The amount of columns is neither 7 nor 8 nor 9 at this column:" -f $computer)
                 Write-Error($_)
             }
         }
@@ -131,9 +114,11 @@ $server+="SVRRDSH03"
 $server+="SVRRDSH04"
 $server+="SVRRDSH05"
 
-$cred = Get-Credential -Message "Please input adminstrative priviledges on the target hosts"
+if (-not $cred) {
+    $cred = Get-Credential -Message "Please input adminstrative priviledges on the target hosts"
+}
 
 if ($cred) {
-    Get-QueryUserSessions -server $server -credentials $cred
+    Get-UserSessions -server $server -credentials $cred
     # Remove-UserSessions -server $server -credentials $cred #-all
 }
