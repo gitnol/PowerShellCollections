0a5813fefb39113a20956ade0e9e825e13e461f3
gitnol <github@noldi.de>
2024-08-13 08:04:57 +0200

    added some functions

--- PSMailstore/Mailstore-Scripts2.ps1
+++ PSMailstore/Mailstore-Scripts2.ps1
@@ -109,41 +109,256 @@ function Set-MSUserEmailAddresses {
 }
 
 # SetUserFullName
+SetUserFullNamefunction Set-MSUserFullName {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$fullName,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $SetUserFullName = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "SetUserFullName" -ApiFunctionParameters @{userName = "$userName"; fullName = "$fullName" }).result
+        $SetUserFullName
+    }
+}
 
 # SetUserLoginPrivileges
+function Set-MSUserLoginPrivileges {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet('none', 'windows', 'web', 'outlook', 'windowsCmd', 'imap', 'api')]
+        [string]$loginPrivileges,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $SetUserLoginPrivileges = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "SetUserLoginPrivileges" -ApiFunctionParameters @{userName = "$userName"; loginPrivileges = "$loginPrivileges" }).result
+        $SetUserLoginPrivileges
+    }
+}
 
 # SetUserPassword
+function Set-MSUserPassword {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$password,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $SetUserPassword = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "SetUserPassword" -ApiFunctionParameters @{userName = "$userName"; password = "$password" }).result
+        $SetUserPassword
+    }
+}
 
 # InitializeMFA
+function Initialize-MSMFA {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $InitializeMFA = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "InitializeMFA" -ApiFunctionParameters @{userName = "$userName" }).result
+        $InitializeMFA
+    }
+}
 
 # DisableMFA
+function Disable-MSMFA {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $DisableMFA = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "DisableMFA" -ApiFunctionParameters @{userName = "$userName" }).result
+        $DisableMFA
+    }
+}
 
 # DeleteAppPasswords
+function Delete-MSAppPasswords {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $DeleteAppPasswords = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "DeleteAppPasswords" -ApiFunctionParameters @{userName = "$userName" }).result
+        $DeleteAppPasswords
+    }
+}
 
 # SetUserPop3UserNames
+function Set-MSUserPop3UserNames {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$pop3UserNames,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $SetUserPop3UserNames = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "SetUserPop3UserNames" -ApiFunctionParameters @{userName = "$userName"; pop3UserNames = "$pop3UserNames" }).result
+        $SetUserPop3UserNames
+    }
+}
 
 # SetUserPrivileges
+function Set-MSUserPrivileges {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet('none', 'admin', 'login', 'changePassword', 'archive', 'modifyArchiveProfiles', 'export', 'modifyExportProfiles', 'delete')]
+        [string]$privileges,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $SetUserPrivileges = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "SetUserPrivileges" -ApiFunctionParameters @{userName = "$userName"; privileges = "$privileges" }).result
+        $SetUserPrivileges
+    }
+}
 
 # RenameUser
+function Rename-MSUser {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$oldUserName,
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$newUserName,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $RenameUser = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "RenameUser" -ApiFunctionParameters @{oldUserName = "$oldUserName"; newUserName = "$newUserName" }).result
+        $RenameUser
+    }
+}
 
 # DeleteUser
+function Delete-MSUser {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $DeleteUser = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "DeleteUser" -ApiFunctionParameters @{userName = "$userName" }).result
+        $DeleteUser
+    }
+}
 
 # SetUserPrivilegesOnFolder
+function Set-MSUserPrivilegesOnFolder {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$folder,
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet('none', 'read', 'write', 'delete')]
+        [string]$privileges,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $SetUserPrivilegesOnFolder = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "SetUserPrivilegesOnFolder" -ApiFunctionParameters @{userName = "$userName"; folder = "$folder"; privileges = "$privileges" }).result
+        $SetUserPrivilegesOnFolder
+    }
+}
 
 # ClearUserPrivilegesOnFolders
+function Clear-MSUserPrivilegesOnFolders {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$userName,
+
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $ClearUserPrivilegesOnFolders = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "ClearUserPrivilegesOnFolders" -ApiFunctionParameters @{userName = "$userName" }).result
+        $ClearUserPrivilegesOnFolders
+    }
+}
 
 #endregion
 
+#region Directory Services
 # # ---------------------------------------------------------------- #
 # # Directory Services                                               #
 # # ---------------------------------------------------------------- #
 # GetDirectoryServicesConfiguration
+function Get-MSDirectoryServicesConfiguration {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        $msapiclient
+    )
+    process {
+        $GetDirectoryServicesConfiguration = (Invoke-MSApiCall -MSApiClient $msapiclient -ApiFunction "GetDirectoryServicesConfiguration").result
+        $GetDirectoryServicesConfiguration
+    }
+}
+
 # SetDirectoryServicesConfiguration
+
 # SyncUsersWithDirectoryServices
+#endregion
+
+#region Credentials
+
 # # ---------------------------------------------------------------- #
 # # Credentials                                                      #
 # # ---------------------------------------------------------------- #
 # GetCredentials
+#endregion
+
 # # ---------------------------------------------------------------- #
 # # Compliance                                                       #
 # # ---------------------------------------------------------------- #
