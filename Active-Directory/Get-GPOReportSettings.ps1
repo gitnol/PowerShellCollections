bf262682b95e2780c859375d3002a20637553517
gitnol <github@noldi.de>
2024-08-16 15:45:04 +0200

    Error Correction

--- Active-Directory/Get-GPOReportSettings.ps1
+++ Active-Directory/Get-GPOReportSettings.ps1
@@ -1,13 +1,14 @@
+Import-Module GroupPolicy
 # This function gets all settings from a GPO. together with "Get-GPO -All"  it is possible to search for settings within the whole domain
 # When you have plenty of GPOs and have no clue which setting is set within which GPO. (without generating an GPResultset for a Computer or user on demand)
 
 function Get-GPOReportSettings {
     param (
+        $gpoName,
         $mynode,
-        $sourceNodeName = "",
-        $sourceNodeName2 = "",
-        $gpoName
+        $sourceNode = @()
     )
+    $sourceNode += $mynode.LocalName
 
     $mynodeName = $mynode.LocalName
     $mynode | Get-Member -MemberType Properties |  ForEach-Object {
@@ -20,8 +21,8 @@ function Get-GPOReportSettings {
                 Name                = $mynodeName
                 mynodePropertyName  = $mynodePropertyName
                 mynodePropertyValue = $mynodePropertyValue 
-                SourceNodeName      = $sourceNodeName
-                SourceNodeName2     = $sourceNodeName2
+                sourceNodeRevert    = ($sourceNode[($sourceNode.count - 1)..0] -join "<-") # Breadcumbs
+                sourceNode          = ($sourceNode -join "->") # Breadcumbs
             }
         }
     }
@@ -29,32 +30,22 @@ function Get-GPOReportSettings {
     if ($mynode.HasChildNodes) {
         # Hat der Node ein Kindelement...
         foreach ($node in $mynode.Childnodes) {
-            # jedes Kindelement an die Funktion übergeben (Rekursion!)
-            # Merke dir die Historie
-            if ($sourceNodeName -eq "") {
-                $historie = $mynode.LocalName
-                $historie2 = $mynode.LocalName
-            }
-            else {
-                $historie = $mynode.LocalName + "<-" + $sourceNodeName
-                $historie2 = $sourceNodeName + "->" + $mynode.LocalName
-            }
+            # # jedes Kindelement an die Funktion übergeben (Rekursion!)
             #rufdichselbstauf
-            checkiechan -mynode $node -sourceNodeName $historie -sourceNodeName2 $historie2 -gpoName $gpoName
+            Get-GPOReportSettings -gpoName $gpoName  -mynode $node -sourceNode $sourceNode
         }
     } 
 }
 
+# Suchbegriff festlegen (Vorsicht bei nur *... das gibt Probleme mit der Datenmenge)
+$suchbegriffSettings = "*Benutzer*"
+
+$LimitToGPOName = "*lauf*"
 
 # Alle GPOs laden (dauert etwas)
-$GPOReport = Get-GPO -All | ForEach-Object {
+$GPOReport = Get-GPO -All | Where-Object DisplayName -like $LimitToGPOName  | ForEach-Object {
     [xml](Get-GPOReport -Guid $_.Id -ReportType Xml)
 }
-
-# Suchbegriff festlegen (Vorsicht bei nur *... das gibt Probleme mit der Datenmenge)
-$suchbegriffSettings = "*Logix*"
-
-$LimitToGPOName = "*Logix*"
 
 # Analyse und Ausgabe der GPOs mit den Suchbegriffen
 $GPOReport | Where-Object { $_.GPO.Name -like $LimitToGPOName } | ForEach-Object { Get-GPOReportSettings  -mynode $_.GPO -gpoName $_.GPO.name } | Where-Object { $_ -like $suchbegriffSettings } | Out-GridView
