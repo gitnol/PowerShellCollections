403895b349801ade754c2b28d2e5a1ed7fee4426
gitnol <github@noldi.de>
2024-12-04 14:09:48 +0100

    minor changes

--- VMware/Get-VMUptimes.ps1
+++ VMware/Get-VMUptimes.ps1
@@ -5,4 +5,4 @@ $stat = 'sys.osuptime.latest'
 $now = Get-Date
 $vms = Get-VM
 # $vms = Get-VM | where{$_.PowerState -eq 'PoweredOn' -and $_.Guest.GuestFamily -match 'windows'}
-Get-Stat -Entity $vms -Stat $stat -Realtime -MaxSamples 1 | Select-Object @{N = 'VM'; E = { $_.Entity.Name } }, @{N = 'OS'; E = { $_.Entity.ExtensionData.Guest.GuestFullName } }, @{N = 'Notes'; E = { $_.Entity.ExtensionData.Summary.Config.Annotation } }, @{N = 'LastOSBoot'; E = { $now.AddSeconds(- $_.Value) } }, @{N = 'UptimeDays'; E = { [math]::Floor($_.Value / (24 * 60 * 60)) } } | Out-GridView
+Get-Stat -Entity $vms -Stat $stat -Realtime -MaxSamples 1 | Select-Object @{N = 'VM'; E = { $_.Entity.Name } }, @{N = 'OS'; E = { $_.Entity.ExtensionData.Guest.GuestFullName } }, @{N = 'LastOSBoot'; E = { $now.AddSeconds(- $_.Value) } }, @{N = 'UptimeDays'; E = { [math]::Floor($_.Value / (24 * 60 * 60)) } }, @{N = 'Notes'; E = { $_.Entity.ExtensionData.Summary.Config.Annotation } } | Out-GridView
