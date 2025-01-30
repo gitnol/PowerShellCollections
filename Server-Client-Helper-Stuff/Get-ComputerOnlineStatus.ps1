65ce0ea0146cb356da6f3ec8bdd040dc098eb910
gitnol <github@noldi.de>
2025-01-30 15:38:28 +0100

    Added IP column and Progress and Status

--- Server-Client-Helper-Stuff/Get-ComputerOnlineStatus.ps1
+++ Server-Client-Helper-Stuff/Get-ComputerOnlineStatus.ps1
@@ -10,7 +10,9 @@ function Test-ConnectionInParallel { # Only Powershell 6 and above (because of F
         [int]$throttlelimit = 10
     )
     $ComputerNames | ForEach-Object -Parallel {
-        [pscustomobject]@{ComputerName=$_;Online=Test-Connection -ComputerName $_ -Count 1 -Quiet -TimeoutSeconds 1}
+        Write-Progress -Activity "Checking computer online status" `
+        -Status "$_"
+        [pscustomobject]@{ComputerName=$_;Online=Test-Connection -ComputerName $_ -Count 1 -Quiet -TimeoutSeconds 1;IP=(Test-Connection -ComputerName $_ -Count 1 -TimeoutSeconds 1 -ErrorAction SilentlyContinue).Address.IPAddressToString}
     } -ThrottleLimit $throttlelimit
 }
 
@@ -57,6 +59,7 @@ function Get-ComputerOnlineStatus {
             [pscustomobject]@{
                 Computer = $using:computer
                 Online   = Test-Connection -ComputerName $using:computer -Count $using:pingCounts -Quiet
+                IP   = (Test-Connection -ComputerName $using:computer -Count 1).Address.IPAddressToString
             }
         }
 
@@ -94,7 +97,7 @@ function Get-ComputerOnlineStatus {
     $jobs | Remove-Job
 
     # Return the results
-    return $results | Select-Object Computer, Online
+    return $results | Select-Object Computer, Online, IP
 }
 
 # Example usage:
