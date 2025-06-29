d30c9de479a988860e6196c0dc14c41e6efb8d81
gitnol <github@noldi.de>
2025-06-29 13:49:30 +0200

    minor changes

--- Server-Client-Helper-Stuff/Get-ComputerOnlineStatus.ps1
+++ Server-Client-Helper-Stuff/Get-ComputerOnlineStatus.ps1
@@ -1,20 +1,20 @@
 # # This is also functioning for powershell 5 where "foreach-object -parallel" is missing
 
 # In Powershell > 6 this function makes it easier
-function Test-ConnectionInParallel { # Only Powershell 6 and above (because of ForEach-Object -Parallel)
+function Test-ConnectionInParallel {
+    # Only Powershell 6 and above (because of ForEach-Object -Parallel)
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $false, Position=0)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $false, Position = 0)]
         [string[]]$ComputerNames,
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory = $false)]
         [int]$throttlelimit = 10
     )
     $ComputerNames | ForEach-Object -Parallel {
         Write-Progress -Activity "Checking computer online status" -Status "$_"
-        [pscustomobject]@{ComputerName=$_;Online=Test-Connection -ComputerName $_ -Count 1 -Quiet -TimeoutSeconds 1;IP=(Test-Connection -ComputerName $_ -Count 1 -TimeoutSeconds 1 -ErrorAction SilentlyContinue).Address.IPAddressToString}
+        [pscustomobject]@{ComputerName = $_; Online = Test-Connection -ComputerName $_ -Count 1 -Quiet -TimeoutSeconds 1; IP = (Test-Connection -ComputerName $_ -Count 1 -TimeoutSeconds 1 -ErrorAction SilentlyContinue).Address.IPAddressToString }
     } -ThrottleLimit $throttlelimit
 }
-
 
 function Get-ComputerOnlineStatus {
     [CmdletBinding()]
@@ -58,7 +58,7 @@ function Get-ComputerOnlineStatus {
             [pscustomobject]@{
                 Computer = $using:computer
                 Online   = Test-Connection -ComputerName $using:computer -Count $using:pingCounts -Quiet
-                IP   = (Test-Connection -ComputerName $using:computer -Count 1).Address.IPAddressToString
+                IP       = (Test-Connection -ComputerName $using:computer -Count 1 -IPv4).Address.IPAddressToString
             }
         }
 
@@ -105,13 +105,12 @@ function Get-ComputerOnlineStatus {
 # $computers = (Get-ADComputer -Filter { OperatingSystem -like '*Windows*' -and Enabled -eq 'True' } -Property DNSHostName).DNSHostName
 # $onlineStatus = Get-ComputerOnlineStatus -Computers $computers
 
-
 $onlyServers = (Get-ADComputer -Filter { OperatingSystem -like '*Windows*' -and OperatingSystem -like '*Server*' -and Enabled -eq 'True' } -Property DNSHostName).DNSHostName
-$onlyServersonlineStatus = Get-ComputerOnlineStatus -Computers ($onlyServers) -numberConcurrentJobs 10 -pingCounts 1
+$onlyServersonlineStatus = Get-ComputerOnlineStatus -Computers ($onlyServers) -numberConcurrentJobs 20 -pingCounts 1
 $onlyServersonlineStatus | Where-Object Online -eq $true |  Out-GridView
 
 $onlyClients = (Get-ADComputer -Filter { OperatingSystem -like '*Windows*' -and OperatingSystem -notlike '*Server*' -and Enabled -eq 'True' } -Property DNSHostName).DNSHostName
-$onlyClientsonlineStatus = Get-ComputerOnlineStatus -Computers ($onlyClients) -numberConcurrentJobs 10 -pingCounts 1
+$onlyClientsonlineStatus = Get-ComputerOnlineStatus -Computers ($onlyClients) -numberConcurrentJobs 20 -pingCounts 1
 # $onlineStatus = Get-ComputerOnlineStatus -Computers ($computers | Select-Object -first 10) -numberConcurrentJobs 10 -pingCounts 1
 $onlyClientsonlineStatus | Where-Object Online -eq $true |  Out-GridView
 
