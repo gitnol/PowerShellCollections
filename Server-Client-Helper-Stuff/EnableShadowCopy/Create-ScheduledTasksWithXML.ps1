fb3ab4fe94471e1daa3be39cacaf8eef6dfa3d8c
Gitnol <git@noldi.de>
2025-07-22 14:11:05 +0200

    Added: Wiederherstellungspunkt Tasks

--- Server-Client-Helper-Stuff/EnableShadowCopy/Create-ScheduledTasksWithXML.ps1
+++ Server-Client-Helper-Stuff/EnableShadowCopy/Create-ScheduledTasksWithXML.ps1
@@ -1,4 +1,5 @@
 schtasks /create /tn "Snapshot_C_Täglich" /xml "Snapshot_C_Täglich.xml" /f
+schtasks /create /tn "Wiederherstellungspunkt_C_Täglich" /xml "Wiederherstellungspunkt_C_Täglich.xml" /f
 
 # vssadmin list shadowstorage
 # vssadmin list shadows
