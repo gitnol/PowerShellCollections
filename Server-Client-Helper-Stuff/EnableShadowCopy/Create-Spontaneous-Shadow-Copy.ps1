3b8df6c9502b9d53e0888b858759800404357b2a
Gitnol <git@noldi.de>
2025-07-22 14:11:40 +0200

    Umgestellt auf CIM und optimiert

--- Server-Client-Helper-Stuff/EnableShadowCopy/Create-Spontaneous-Shadow-Copy.ps1
+++ Server-Client-Helper-Stuff/EnableShadowCopy/Create-Spontaneous-Shadow-Copy.ps1
@@ -10,16 +10,20 @@ function New-CShadowCopyLink {
     }
 
     Write-Host "Erzeuge VSS-Instanz für $Volume ..."
-    $vssObject = [WmiClass]"root\cimv2:Win32_ShadowCopy"
-    $result = $vssObject.Create($Volume, "ClientAccessible")
+    $vssClass = Get-CimClass -Namespace root/cimv2 -ClassName Win32_ShadowCopy
+    $createParams = @{
+        Volume  = $Volume
+        Context = "ClientAccessible"
+    }
+    $result = Invoke-CimMethod -CimClass $vssClass -MethodName Create -Arguments $createParams
 
     if ($result.ReturnValue -ne 0) {
         Write-Error "VSS-Erstellung fehlgeschlagen. Fehlercode: $($result.ReturnValue)"
         return
     }
 
-    $shadowID = [string]$result.ShadowID
-    $snapshot = Get-WmiObject Win32_ShadowCopy | Where-Object { $_.ID -eq $shadowID }
+    $shadowID = $result.ShadowID.ToString()
+    $snapshot = Get-CimInstance -ClassName Win32_ShadowCopy -Filter "ID='$shadowID'"
 
     if (-not $snapshot) {
         Write-Error "Snapshot nicht gefunden."
@@ -33,15 +37,19 @@ function New-CShadowCopyLink {
     cmd.exe /c "mklink /d `"$LinkPath`" `"$deviceObject`"" | Out-Null
 
     Write-Host "Schattenkopie ist bereit unter $LinkPath. Vorgang abschließen und beliebige Eingabetaste drücken ..."
+    Start-Process -Wait -FilePath "explorer.exe" -ArgumentList @("$LinkPath")
     Pause
 
     Write-Host "Entferne symbolischen Link ..."
     [System.IO.Directory]::Delete($LinkPath, $true)
 
     Write-Host "Lösche VSS-Instanz ..."
-    $snapshot.Delete() | Out-Null
+    $snapshot | Remove-CimInstance -ErrorAction Stop
 
     Write-Host "Fertig."
 }
 
+# Beispielaufruf:
+# Erstelle einen symbolischen Link für die Schattenkopie von C:\ unter C:\vsslink
+# Hinweis: Stelle sicher, dass der Pfad C:\vsslink nicht bereits existiert
 # New-CShadowCopyLink -Volume "C:\" -LinkPath "C:\vsslink"
