e80d01515626f56c5ed2c0cbb22f7f83917357b8
Gitnol <git@noldi.de>
2025-09-15 14:14:37 +0200

    added ldaps bind funktion

--- Active-Directory/Test-LdapPorts.ps1
+++ Active-Directory/Test-LdapPorts.ps1
@@ -35,5 +35,47 @@ function Test-LdapPorts {
     $results | Format-Table -AutoSize
 }
 
+function Test-LdapsBind {
+    param(
+        [string]$Server = "192.168.1.100",
+        [int]$Port = 636,
+        [pscredential]$Cred
+    )
+
+    try {
+        $entry = New-Object System.DirectoryServices.DirectoryEntry(
+            "LDAP://$Server`:$Port",
+            $Cred.UserName,
+            $Cred.GetNetworkCredential().Password,
+            [System.DirectoryServices.AuthenticationTypes]::SecureSocketsLayer
+        )
+
+        # Versuch auf NativeObject → löst Bind aus
+        $null = $entry.NativeObject
+
+        [PSCustomObject]@{
+            Server  = $Server
+            Port    = $Port
+            User    = $Cred.UserName
+            Success = $true
+            Message = "LDAPS-Bind erfolgreich"
+        }
+    }
+    catch {
+        [PSCustomObject]@{
+            Server  = $Server
+            Port    = $Port
+            User    = $Cred.UserName
+            Success = $false
+            Message = $_.Exception.Message
+        }
+    }
+}
+
+
+$cred = Get-Credential
+Test-LdapsBind -Server "192.168.1.100" -Cred $cred
+
+
 # Beispielaufruf
 Test-LdapPorts -TargetIP '192.168.1.100'
