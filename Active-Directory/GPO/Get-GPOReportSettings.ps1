58bb1f1518821aa68b6ff45c78cfc86fdf266d2a
Gitnol <git@noldi.de>
2025-11-14 14:12:15 +0100

    Major changes

--- Active-Directory/GPO/Get-GPOReportSettings.ps1
+++ Active-Directory/GPO/Get-GPOReportSettings.ps1
@@ -1,106 +1,164 @@
 Import-Module GroupPolicy # If this does not work with Powershell 7, try it as admin / elevated
 # This function gets all settings from a GPO. together with "Get-GPO -All"  it is possible to search for settings within the whole domain
 # When you have plenty of GPOs and have no clue which setting is set within which GPO. (without generating an GPResultset for a Computer or user on demand)
+ 
+# Version 5.0: Hybrid-Ansatz für vollständige Abdeckung.
+# - Kombiniert die intelligente Zusammenfassung von ADMX-Richtlinien (`<Policy>`-Knoten) mit einer generischen,
+#   rekursiven Analyse für alle anderen GPO-Einstellungen (Preferences, Security Settings etc.).
+# - Stellt sicher, dass keine Einstellung im GPO-Bericht übersehen wird.
+
 
 function Get-GPOReportSettings {
+    [CmdletBinding()]
     param (
-        $gpoName,
-        $mynode, # Dies ist der aktuelle XML-Knoten (z.B. <q1:EditText>)
-        $sourceNode = @()
+        [Parameter(Mandatory = $true)]
+        [string]$GpoName,
+
+        [Parameter(Mandatory = $true)]
+        [System.Xml.XmlElement]$XmlNode
     )
-    # Beachte: Beim Start des Skriptblocks sollte für $sourceNode 
-    # nur der Wert des aktuellen Knotens hinzugefügt werden.
-    # Wenn $mynode ein XML-Node ist, ist LocalName passend.
-    $sourceNode += $mynode.LocalName
 
-    # Wir verwenden Get-Member auf dem XML-Knoten, um alle direkten Unterelemente
-    # und Attribute als Properties zu finden, die Strings sein könnten.
-    $mynode | Get-Member -MemberType Properties | ForEach-Object {
-        # Wir filtern nach 'string' Properties, die oft direkt den Textinhalt 
-        # eines Kindknotens repräsentieren, ODER nach XML-Knoten selbst.
-        # Im GPO-Kontext können Properties wie 'Name', 'State', 'Value' Strings 
-        # oder Kind-XML-Nodes sein, die als String zurückgegeben werden sollen.
-        # Da Get-Member für diese Childnodes oft 'string' ausgibt, 
-        # bleiben wir bei der Prüfung auf "string" oder "System.Xml.XmlElement".
-        
-        $isStringProp = $_.Definition.StartsWith("string") 
-        $isXmlElementProp = $_.TypeName -eq 'System.Xml.XmlElement'
-        
-        if ($isStringProp -or $isXmlElementProp) {
-            $mynodePropertyName = $_.Name
-            
-            # 1. Versuche den Wert direkt zu holen. Wenn das ChildNode leer ist (z.B. <q1:Name/>), 
-            #    liefert $mynode."$mynodePropertyName" oft $null.
-            $mynodePropertyValue = $mynode."$mynodePropertyName"
+    # Interne Hilfsfunktion, um ADMX-basierte Richtlinien sauber zu parsen.
+    function ConvertFrom-PolicyNode {
+        param($policyNode)
 
-            # 2. Wenn $mynodePropertyValue $null ist, ist dies ein starker Hinweis darauf, 
-            #    dass der Child-Knoten existiert, aber leer ist. Wir müssen explizit 
-            #    .InnerText des Child-Knotens abrufen, um einen leeren String zu erhalten.
-            if ($null -eq $mynodePropertyValue) { 
-                # Versuche, den tatsächlichen XML-ChildNode zu finden
-                $childNode = $mynode.$mynodePropertyName
-                
-                # Wenn der ChildNode existiert, aber keinen Wert hatte (wie bei <q1:Name />),
-                # dann liefert .InnerText den leeren String "".
-                if ($null -ne $childNode) {
-                    $mynodePropertyValue = $childNode.InnerText
+        $policyName = $policyNode.SelectSingleNode("*[local-name()='Name']")?.InnerText
+        $policyState = $policyNode.SelectSingleNode("*[local-name()='State']")?.InnerText
+        $policyCategory = $policyNode.SelectSingleNode("*[local-name()='Category']")?.InnerText
+        $policyExplain = $policyNode.SelectSingleNode("*[local-name()='Explain']")?.InnerText
+
+        # NEU: Ermitteln, ob es sich um eine Computer- oder Benutzerrichtlinie handelt.
+        $configType = "Unbekannt"
+        if ($policyNode.SelectSingleNode("ancestor::*[local-name()='Computer']")) {
+            $configType = "Computer"
+        }
+        elseif ($policyNode.SelectSingleNode("ancestor::*[local-name()='User']")) {
+            $configType = "Benutzer"
+        }
+
+        $childSettingNodes = $policyNode.SelectNodes("*[local-name()!='Name' and local-name()!='State' and local-name()!='Category' and local-name()!='Explain' and local-name()!='Supported' and local-name()!='Text']")
+
+        $settingsList = foreach ($node in $childSettingNodes) {
+            $settingName = $node.SelectSingleNode("*[local-name()='Name']")?.InnerText
+            $settingValue = ''
+
+            switch ($node.LocalName) {
+                'EditText' { $settingValue = $node.SelectSingleNode("*[local-name()='Value']")?.InnerText }
+                'CheckBox' { $settingValue = $node.SelectSingleNode("*[local-name()='State']")?.InnerText }
+                'DropDownList' { $settingValue = $node.SelectSingleNode("*[local-name()='Value']/*[local-name()='Name']")?.InnerText }
+                'ListBox' {
+                    $listItems = $node.SelectNodes(".//*[local-name()='Data']") | ForEach-Object { $_.InnerText }
+                    $settingValue = $listItems -join ', '
                 }
-                else {
-                    # Falls es aus irgendeinem Grund $null bleibt, explizit auf leeren String setzen
-                    $mynodePropertyValue = [string]::Empty
+                default {
+                    $settingValue = $node.SelectSingleNode("*[local-name()='Value']")?.InnerText
                 }
-            } 
-            
-            # WICHTIG: Wenn der Wert jetzt noch $null ist, war der Property-Name möglicherweise 
-            # nicht vorhanden. Aber in den meisten Fällen, wo Get-Member es meldet, 
-            # sollte er jetzt ein String oder ein leerer String sein.
-            if ([string]::IsNullOrEmpty($mynodePropertyValue)) {
-                $mynodePropertyValue = [string]::Empty
             }
 
+            if ([string]::IsNullOrWhiteSpace($settingName)) {
+                $settingName = $node.LocalName
+            }
 
-            # Nodeinfos zurückgeben
+            "$settingName = $settingValue"
+        }
+
+        [pscustomobject]@{
+            GPOName           = $GpoName
+            PolicyName        = $policyName
+            PolicyState       = $policyState
+            CategoryPath      = $policyCategory
+            Konfigurationstyp = $configType # NEUE Spalte
+            Settings          = $settingsList -join '; '
+            Explain           = $policyExplain
+            Type              = "Policy"
+            Path              = "GPO -> " + ($policyCategory -replace '/', ' -> ')
+        }
+    }
+
+    # Interne, rekursive Funktion für alle anderen Einstellungen.
+    function Get-GenericSettings {
+        param($node, $currentPath)
+
+        # 1. Attribute des Knotens ausgeben
+        foreach ($attribute in $node.Attributes) {
             [pscustomobject]@{
-                GPOName             = $gpoName
-                Name                = $mynode.LocalName
-                mynodePropertyName  = $mynodePropertyName
-                mynodePropertyValue = $mynodePropertyValue # <- Jetzt ist es ein String oder ein leerer String
-                sourceNodeRevert    = ($sourceNode[($sourceNode.count - 1)..0] -join "<-")
-                sourceNode          = ($sourceNode -join "->")
+                GPOName           = $GpoName
+                PolicyName        = $node.LocalName
+                PolicyState       = $null
+                Konfigurationstyp = $null # Nicht relevant für generische Einstellungen
+                CategoryPath      = $null
+                Settings          = "$($attribute.Name) = $($attribute.Value)"
+                Explain           = $null # KORREKTUR: Eigenschaft für konsistente Ausgabe hinzufügen
+                Type              = 'Attribute'
+                Path              = $currentPath -join ' -> '
+            }
+        }
+
+        # 2. Kindknoten rekursiv durchlaufen
+        $childNodes = $node.ChildNodes | Where-Object { $_.NodeType -eq 'Element' }
+        if ($childNodes.Count -gt 0) {
+            foreach ($childNode in $childNodes) {
+                # WICHTIG: Wenn wir einen 'Policy'-Knoten finden, stoppen wir die Rekursion hier,
+                # da dieser separat behandelt wird.
+                if ($childNode.LocalName -ne 'Policy') {
+                    Get-GenericSettings -node $childNode -currentPath ($currentPath + $childNode.LocalName)
+                }
+            }
+        }
+        # 3. Textwert des Endknotens ausgeben
+        else {
+            if (-not [string]::IsNullOrWhiteSpace($node.InnerText)) {
+                [pscustomobject]@{
+                    GPOName           = $GpoName
+                    PolicyName        = $node.ParentNode.LocalName
+                    PolicyState       = $null
+                    Konfigurationstyp = $null # Nicht relevant für generische Einstellungen
+                    CategoryPath      = $null
+                    Settings          = "$($node.LocalName) = $($node.InnerText.Trim())"
+                    Explain           = $null # KORREKTUR: Eigenschaft für konsistente Ausgabe hinzufügen
+                    Type              = 'Value'
+                    Path              = $currentPath -join ' -> '
+                }
             }
         }
     }
 
-    # Rekursion für ChildNodes
-    if ($mynode.HasChildNodes) {
-        # Hat der Node ein Kindelement...
-        foreach ($node in $mynode.Childnodes) {
-            # Da der XML-Knoten $mynode.Childnodes auch Text-Knoten enthält,
-            # prüfen wir, ob es sich um ein Element handelt.
-            if ($node.NodeType -eq [System.Xml.XmlNodeType]::Element) {
-                # jedes Kindelement an die Funktion übergeben (Rekursion!)
-                Get-GPOReportSettings -gpoName $gpoName -mynode $node -sourceNode $sourceNode
-            }
-        }
-    } 
-    # Nach der Rekursion den aktuellen Node aus $sourceNode entfernen (wird aber im 
-    # nächsten Aufruf des $sourceNode-Parameters automatisch korrekt gehandhabt, 
-    # solange $sourceNode als Parameter übergeben wird).
+    # --- HAUPTAUSFÜHRUNG ---
+    # KORREKTUR: Reihenfolge geändert, um sicherzustellen, dass das erste Objekt alle Spalten hat.
+    # 1. Zuerst die reichhaltigen ADMX-basierten Richtlinien parsen.
+    $policyNodes = $XmlNode.SelectNodes(".//*[local-name()='Policy']")
+    foreach ($policyNode in $policyNodes) {
+        ConvertFrom-PolicyNode -policyNode $policyNode
+    }
+
+    # 2. Danach alle anderen Einstellungen generisch und rekursiv durchlaufen.
+    Get-GenericSettings -node $XmlNode -currentPath @($XmlNode.LocalName)
 }
 
 # Suchbegriff festlegen (Vorsicht bei nur *... das gibt Probleme mit der Datenmenge)
-$suchbegriffSettings = "*Benutzer*"
+$suchbegriffSettings = "*Startseite*"
 
 # Begrenze es auf eine GPO mit dem folgendem Namen. "*" als Wildcard nutzbar"
-$LimitToGPOName = "*lauf*"
+$LimitToGPOName = "*Firefox*"
 
 # Ab brauch nichts mehr verändert werden:
 # Alle GPOs laden (dauert etwas)
-$GPOReport = Get-GPO -All | Where-Object DisplayName -like $LimitToGPOName  | ForEach-Object {
-    [xml](Get-GPOReport -Guid $_.Id -ReportType Xml)
+$GPOs = Get-GPO -All | Where-Object { $_.DisplayName -like $LimitToGPOName }
+
+Write-Host "Analysiere $($GPOs.Count) GPO(s)..."
+
+# Die Ergebnisse aller GPO-Analysen in einer Variablen sammeln
+$allSettings = foreach ($gpo in $GPOs) {
+    Write-Host "Verarbeite GPO: $($gpo.DisplayName)"
+    $xmlString = Get-GPOReport -Guid $gpo.Id -ReportType Xml
+    $xmlReport = [xml]$xmlString
+
+    # Wir rufen die Funktion für den gesamten GPO-Knoten auf.
+    # Die Funktion durchsucht dann selbstständig alle Unterbereiche (Computer/User).
+    Get-GPOReportSettings -GpoName $gpo.DisplayName -XmlNode $xmlReport.GPO
 }
 
 # Analyse und Ausgabe der GPOs mit den Suchbegriffen
-$GPOReport | Where-Object { $_.GPO.Name -like $LimitToGPOName } | ForEach-Object { Get-GPOReportSettings  -mynode $_.GPO -gpoName $_.GPO.name } | Where-Object { $_ -like $suchbegriffSettings } | Out-GridView
-# Alles - Sehr langsam bei sehr vieen GPOs
-# $GPOReport | Where-Object { $_.GPO.Name -like $LimitToGPOName } | ForEach-Object { Get-GPOReportSettings  -mynode $_.GPO -gpoName $_.GPO.name } | Out-GridView
+$allSettings | Where-Object { $_.PolicyName -like $suchbegriffSettings -or $_.Settings -like "*$suchbegriffSettings*" -or $_.CategoryPath -like "*$suchbegriffSettings*" } | Out-GridView
+
+Write-Host "Analyse abgeschlossen."
