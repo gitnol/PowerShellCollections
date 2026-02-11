ad01082d2224109c1039eae64e3a7a2e9043c72d
Gitnol <git@noldi.de>
2026-02-11 10:04:31 +0100

    remove All-Inkl-API (moved to github.com/gitnol/all-inkl-kas-sdk)

--- All-Inkl-API/kas_sdk/__init__.py
+++
@@ -1,3 +0,0 @@
-from .client import KasClient
-
-__all__ = ["KasClient"]
--- All-Inkl-API/kas_sdk/services/base.py
+++
@@ -1,8 +0,0 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from ..client import KasClient
-
-class BaseService:
-    def __init__(self, client: 'KasClient'):
-        self.client = client
